55b519c6eab0e0f7155200a1e1372651beb0ce73
Besforti10 <78754787+Besforti10@users.noreply.github.com>
2024-05-23 14:11:58 +0200

    Add files via upload

---
+++ Task-1/dynamodb/dynamodb.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "besfort_table" {
+  name           = "besfort-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "besfort_table2" {
+  name           = "besfort-table2"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
